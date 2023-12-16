fc1ebe79c972d7cb59efff0fba6a99ab09e6c940
Ricardo5005 <ricardoizidoro2009@hotmail.com>
2023-12-16 18:37:29 -0300

    estruturação de cards

--- post_description/js/post_page.js
+++ post_description/js/post_page.js
@@ -19,39 +19,39 @@ const indice_post = localStorage.getItem('card_indice') || '0';
 // renderizar post individual------
 switch (indice_post) {
     case "1":
-        title.innerHTML = "titulo do card 1";
+        title.innerHTML = "playstation 5";
         date_time.innerHTML = `${date} - ${time}`;
-        category.innerHTML = "categoria do card 1";
+        category.innerHTML = "Game";
         views.innerHTML = views_card_1;
-        img.setAttribute("src", "https://images.unsplash.com/photo-1670332339459-fe5d2093167a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=60");
-        text.innerHTML = "aqui jaz o texto do card 1"
+        img.setAttribute("src", "https://images.unsplash.com/photo-1617864064479-f203fc7897c0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheXN0YXRpb24lMjA1fGVufDB8fDB8fHww");
+        text.innerHTML = "Os principais recursos de hardware do PlayStation 5 incluem uma unidade de estado sólido personalizada para streaming de dados em alta velocidade para permitir melhorias significativas no desempenho de armazenamento, uma AMD GPU capaz de resolução 4Krastreamento de raio e com . PlayStation VR2PlayStation VR com a maioria dos jogos de PlayStation 4 e compatibilidade com versões anteriores, feedback tátil com DualSense acelerados por hardware. Outros recursos incluem o controlador efeitos de áudio 3D para iluminação e reflexos realistas, e o Tempest Engine para , quadros por segundo exibição em até 120"
         break;
        
         case "2":
-        title.innerHTML = "titulo do card2";
+        title.innerHTML = "smartphone";
         date_time.innerHTML = `${date} - ${time}`;
-        category.innerHTML = "categoria do card 2";
+        category.innerHTML = "mobile";
         views.innerHTML = views_card_2;
-        img.setAttribute("src", "https://images.unsplash.com/photo-1686139280426-483b26ab94b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=60");
-        text.innerHTML = "aqui jaz o texto do card 1"
+        img.setAttribute("src","https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        text.innerHTML = "Telefone celular (português brasileiro) ou telemóvel (português europeu) é um aparelho de comunicação por ondas eletromagnéticas que permite a transmissão bidirecional de voz e dados utilizáveis em uma área geográfica que se encontra dividida em células (de onde provém a nomenclatura celular), cada uma delas servida por um transmissor/receptor. A invenção do telefone celular ocorreu em 1947 pelo laboratório Bell, nos Estados Unidos."
         break;
       
         case "3":
-            title.innerHTML = "titulo do card3";
+            title.innerHTML = "smartwatch";
             date_time.innerHTML = `${date} - ${time}`;
-            category.innerHTML = "categoria do card 3";
+            category.innerHTML = "iot";
             views.innerHTML = views_card_3;
-            img.setAttribute("src", "https://images.unsplash.com/photo-1621246046602-e75168c03fd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=60");
-            text.innerHTML = "aqui jaz o texto do card 3"
+            img.setAttribute("src", "https://images.unsplash.com/photo-1579586337278-3befd40fd17a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D");
+            text.innerHTML = "Um relógio inteligente é um relógio de pulso computadorizado com funcionalidades que vão além de mostrar as horas, e é com frequência comparado aos assistentes digitais pessoais (PDA). Enquanto os primeiro modelos podem executar tarefas simples, como cálculos, tradução e jogar videojogos, os relógios inteligentes modernos são efetivamente computadores vestíveis."
             break;
            
             case "4":
-                title.innerHTML = "titulo do card4";
+                title.innerHTML = "veiculo elétrico";
                 date_time.innerHTML = `${date} - ${time}`;
-                category.innerHTML = "categoria do card 4";
+                category.innerHTML = "sustentabilidade";
                 views.innerHTML = views_card_4;
-                img.setAttribute("src", "https://images.unsplash.com/photo-1691938608898-88ae4d41375c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=2000&q=60");
-                text.innerHTML = "aqui jaz o texto do card 4"
+                img.setAttribute("src","https://images.unsplash.com/photo-1615829386703-e2bb66a7cb7d?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+                text.innerHTML = "Carro elétrico é um veículo a motor para transporte de carga e de pessoas propulsionado por motor elétrico.                O termo descreve carros de passeio em geral, mas pode, contudo, também ser entendido para toda a gama de veículos de várias faixas"
                 break;
         
 
